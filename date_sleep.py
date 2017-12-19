19bcf3c70a4b8c6b9e75b9b68de469b2dcdb9bd4
Charlie Voiselle <charlie.voiselle@gmail.com>
2017-12-19 12:48:55 -0500

    Changed print to use stdout.write; typo fix

--- date_sleep.py
+++ date_sleep.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # This is super basic and should run on prety-much any python you happen to have.
 
-# This is a simple python program that loops forever printing the dave every 2 seconds.
+# This is a simple python program that loops forever printing the date every 2 seconds.
 import time
 from datetime import datetime
 
 while True:
 	# print date
 	i = datetime.now()
- 	print str(i)
+ 	sys.stdout.write(str(i)+'\n')
 	# sleep 2 seconds
 	time.sleep(2)
